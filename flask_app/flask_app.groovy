4e3ed8ff13213e5ddcd2bada523b28f96c8574ee
Hari prakash <hariprakashhp5@gmail.com>
2020-02-21 16:02:49 +0530

    Update flask_app.groovy

--- flask_app/flask_app.groovy
+++ flask_app/flask_app.groovy
@@ -37,7 +37,7 @@ def imagePrune(containerName){
 }
 
 def imageBuild(containerName, tag){
-    sh "docker build -t $containerName:$tag  -t $containerName --pull --no-cache ./flask-app/"
+    sh "docker build -t $containerName:$tag --no-cache ./flask_app/"
     echo "Image build complete"
 }
 
