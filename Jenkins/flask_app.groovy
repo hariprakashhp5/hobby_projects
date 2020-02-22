f1db61e767e113425c470881787b5dd1fa4c80a3
hariprakashhp5 <hariprakashhp5>
2020-02-23 01:24:00 +0530

    reverted multistage docker file and added new docker file for grafana

--- Jenkins/flask_app.groovy
+++ Jenkins/flask_app.groovy
@@ -34,7 +34,7 @@ def imagePrune(containerName){
 }
 
 def imageBuild(containerName, tag){
-    sh "docker build --target $containerName -t $containerName:$tag -f Docker/Dockerfile ."
+    sh "docker build -t $containerName:$tag -f Docker/$containerName_dockerfile ."
     echo "Image build complete"
 }
 
