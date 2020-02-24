2d66035d60e061d2123c59d5c9f3b8048acb1a38
hprakash.contractor <hprakash.contractor@libertyglobal.com>
2020-02-24 12:16:44 +0530

    updates 13

--- Jenkins/test.groovy
+++ Jenkins/test.groovy
@@ -1,15 +1,13 @@
 node {
     def dockerHome = tool 'myDocker'
     env.PATH = "${dockerHome}/bin:${env.PATH}"
-    pipeline {
-        agent {
-            docker { image 'node:7-alpine' }
-        }
-        stages {
-            stage('Test') {
-                steps {
-                    sh 'node --version'
-                }
+    agent {
+        docker { image 'node:7-alpine' }
+    }
+    stages {
+        stage('Test') {
+            steps {
+                sh 'node --version'
             }
         }
     }
