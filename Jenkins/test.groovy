6f888edfa43c1cfddb5a5b7c6d3c4e748d3e71c6
hariprakashhp5 <hariprakashhp5>
2020-02-24 13:11:20 +0530

    updates 21

--- Jenkins/test.groovy
+++ Jenkins/test.groovy
@@ -13,6 +13,12 @@ pipeline {
         stage('Build Flask App Image') {
             steps {
                sh "docker build -t flask_app:latest -f Docker/flask_app.df ."
+               step([
+                    $class: 'DockerComposeBuilder',
+                    dockerComposeFile: 'Docker/docker-compose.yml',
+                    option: [$class: 'StartService', scale: 1, service: 'flask_app'],
+                    useCustomDockerComposeFile: true
+               ])
             }
         }
 
