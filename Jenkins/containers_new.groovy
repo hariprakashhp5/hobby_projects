7681422308d48912cb4e265891aacd66765a686a
hariprakashhp5 <hariprakashhp5>
2020-02-24 14:26:57 +0530

    updates 24

--- Jenkins/containers_new.groovy
+++ Jenkins/containers_new.groovy
@@ -1,41 +1,63 @@
 pipeline {
     agent {label 'master'}
-	parameters {
-        booleanParam(name: 'flask_app', defaultValue: true, description: 'Build Generic Python Backend application Image')
-        booleanParam(name: 'grafana', defaultValue: true, description: 'Build Grafana Image')
-        booleanParam(name: 'clickhouse', defaultValue: true, description: 'Build Clickhouse DB Image')
+    parameters {
+        booleanParam(name: 'flask_app', defaultValue: true, description: 'Build Generic Python Backend application Image & spin a container')
+        booleanParam(name: 'grafana', defaultValue: true, description: 'Build Grafana Image & spin a container')
+        booleanParam(name: 'rsyslog', defaultValue: true, description: 'Create rSyslog Container')
+        booleanParam(name: 'clickhouse', defaultValue: true, description: 'Build Clickhouse DB Image & spin a container')
     }
     stages {
 		stage('Initialize'){
-		    def dockerHome = tool 'myDocker'
-            env.PATH = "${dockerHome}/bin:${env.PATH}"
+		    steps {
+		        script {
+		            def dockerHome = tool 'myDocker'
+                    env.PATH = "${dockerHome}/bin:${env.PATH}"
+		        }
+		    }
         }
 
         stage('Build Flask App Image') {
-			when { expression { params.flask_app } }
-			agent {
-				dockerfile {
-					filename 'Docker/flask_app.df'
-					dir '.'
-					args '-t flask_app:latest'
-				}			
-			}
+            when { expression { params.flask_app } }
             steps {
-               echo env.PATH
-               echo 'Done!'
+               sh "docker build -t flask_app:latest -f Docker/flask_app.df ."
+               step([
+                    $class: 'DockerComposeBuilder',
+                    dockerComposeFile: 'Docker/docker-compose.yml',
+                    option: [$class: 'StartService', scale: 1, service: 'flask_app'],
+                    useCustomDockerComposeFile: true
+               ])
             }
         }
-		stage('Build Grafana Image') {
-			when { expression { params.grafana } }
-			agent {
-				dockerfile {
-					filename 'Docker/grafana.df'
-					dir '.'
-					args '-t v6.6.2'
-				}			
-			}
+
+        stage('Build Grafana Image') {
+            when { expression { params.grafana } }
             steps {
-               echo 'Done'
+               sh "docker build -t grafana:v6.6.2 -f Docker/grafana.df ."
+               step([
+                    $class: 'DockerComposeBuilder',
+                    dockerComposeFile: 'Docker/docker-compose.yml',
+                    option: [$class: 'StartService', scale: 1, service: 'grafana'],
+                    useCustomDockerComposeFile: true
+               ])
+            }
+        }
+
+        stage('Spinup rSyslog') {
+            when { expression { params.rsyslog } }
+            steps {
+               step([
+                    $class: 'DockerComposeBuilder',
+                    dockerComposeFile: 'Docker/docker-compose.yml',
+                    option: [$class: 'StartService', scale: 1, service: 'rsyslog'],
+                    useCustomDockerComposeFile: true
+               ])
+            }
+        }
+
+        stage('Build Clickhouse Image') {
+            when { expression { params.clickhouse } }
+            steps {
+               echo "YTD"
             }
         }
     }
