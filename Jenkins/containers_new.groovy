e489a64b2ef726fb0df8ca3a4ea518e84df3dd5f
hariprakashhp5 <hariprakashhp5>
2020-02-27 10:51:32 +0530

    added build args for grafana

--- Jenkins/containers_new.groovy
+++ Jenkins/containers_new.groovy
@@ -3,6 +3,8 @@ pipeline {
     parameters {
         booleanParam(name: 'flask_app', defaultValue: true, description: 'Build Generic Python Backend application Image & spin a container')
         booleanParam(name: 'grafana', defaultValue: true, description: 'Build Grafana Image & spin a container')
+        string(name: 'GUAI', defaultValue: '', description: 'Google Analytics UA ID')
+        string(name: 'GTMI', defaultValue: '', description: 'Google Tag Manager ID')
         booleanParam(name: 'rsyslog', defaultValue: true, description: 'Create rSyslog Container')
         booleanParam(name: 'clickhouse', defaultValue: true, description: 'Build Clickhouse DB Image & spin a container')
     }
@@ -32,7 +34,7 @@ pipeline {
         stage('Build Grafana Image') {
             when { expression { params.grafana } }
             steps {
-               sh "docker build -t grafana:v6.6.2 -f Docker/grafana.df ."
+               sh "docker build --build-arg GUAI=params.GUAI --build-arg GTMI=params.GTMI -t grafana:v6.6.2 -f Docker/grafana.df ."
                step([
                     $class: 'DockerComposeBuilder',
                     dockerComposeFile: 'Docker/docker-compose.yml',
